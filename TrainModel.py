9aad5e478001bffb48beb8c8f76d73f882b7ba9b
Vedant Ganesh <vedant.ganesh@gmail.com>
2025-09-18 22:09:26 +0530

    I cannot download this large ass model

--- TrainModel.py
+++ TrainModel.py
@@ -1,10 +1,26 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline #type: ignore
+import os
+from dotenv import load_dotenv #type: ignore
 
-model_name = "google/vaultgemma-1b"
+load_dotenv()
+HF_TOKEN = os.getenv('HF_TOKEN')
+
+# print(HF_TOKEN)
+
+model_name = "google/gemma-2b"
 
 # Load the pre-trained tokeniser and model from HuggingFace
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(
+    model_name,
+    token=HF_TOKEN
+    )
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    token=HF_TOKEN,
+    torch_dtype="auto", 
+    device_map="auto", 
+    trust_remote_code=True
+    )
 
 # Querying
 text = "Tell me an unknown interesting biology fact about the brain."
